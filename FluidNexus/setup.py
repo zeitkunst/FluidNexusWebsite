7f9aba47412812b19c2be7e33ac7ff5d6ce4e85c
zeitkunst <nknouf@zeitkunst.org>
2011-08-17 02:56:01 -0400

    -- updating credits page with mailing list info; adding links to bugtracker

--- FluidNexus/setup.py
+++ FluidNexus/setup.py
@@ -38,7 +38,7 @@ if sys.version_info[:3] < (2,5,0):
     requires.append('pysqlite')
 
 setup(name='FluidNexus',
-      version='0.1.0',
+      version='0.2.0',
       description='FluidNexus',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
